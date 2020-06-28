9aca9e98ec608f319f113952b02d3f766adcfb42
alenkruth <alenkruth@gmail.com>
2020-06-28 14:32:35 +0530

    updates

---
+++ src/MixColumnError.sv
@@ -0,0 +1,16 @@
+	   /*data_o[(32*i)+:8]      = MultiplyTwo(data_i[(32*i)+:8]) ^
+                                     MultiplyThree(data_i[((32*i)+8)+:8]) ^
+                                     data_i[((32*i)+16)+:8] ^
+                                     data_i[((32*i)+24)+:8];
+            data_o[((32*i)+8)+:8]  = data_i[(32*i)+:8] ^
+                                     MultiplyTwo(data_i[((32*i)+8)+:8]) ^
+                                     MultiplyThree(data_i[((32*i)+16)+:8]) ^
+                                     data_i[((32*i)+24)+:8];
+            data_o[((32*i)+16)+:8] = data_i[(32*i)+:8] ^
+                                     data_i[((32*i)+8)+:8] ^
+                                     MultiplyTwo(data_i[((32*i)+16)+:8]) ^
+                                     MultiplyThree(data_i[((32*i)+24)+:8]);
+            data_o[((32*i)+24)+:8] = MultiplyThree(data_i[(32*i)+:8]) ^
+                                     data_i[((32*i)+8)+:8] ^
+                                     data_i[((32*i)+16)+:8] ^
+                                     MultiplyTwo(data_i[((32*i)+24)+:8]);*/
