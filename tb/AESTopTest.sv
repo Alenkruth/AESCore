100c901847b2c2b931df0d92df7c66a5ddfffbb0
alenkruth <alenkruth@gmail.com>
2020-06-28 21:12:14 +0530

    updated .gitignore

--- tb/AESTopTest.sv
+++ tb/AESTopTest.sv
@@ -46,15 +46,15 @@ module AESTopTest(
         enable = 1'b1;
         ///////////////////////////////////////////////////////
         // test vectors from the AES specfication NIST.FIPS 197
-        plaintext_test = 128'h00112233445566778899aabbccddeeff;
-        key_test = 256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
+        // plaintext_test = 128'h00112233445566778899aabbccddeeff;
+        // key_test = 256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
         // the cipher text is 8ea2b7ca516745bfeafc49904b496089
         //////////////////////////////////////////////////////
 
         ///////////////////////////////////////////////////////
         // custom test vectors
-        // plaintext_test = 128'h616c656e6b72757468616c656e6b7275; // alenkruthalenkru in ASCII
-        // key_test = 256'h7465737474657374746573747465737474657374746573747465737474657374; // testtesttesttesttesttesttesttest in ASCII
+        plaintext_test = 128'h616c656e6b72757468616c656e6b7275; // alenkruthalenkru in ASCII
+        key_test = 256'h7465737474657374746573747465737474657374746573747465737474657374; // testtesttesttesttesttesttesttest in ASCII
         // the cipher text is 4419ce8172f99fa38dc6119260edb3f8
         ///////////////////////////////////////////////////////
         #4000;
