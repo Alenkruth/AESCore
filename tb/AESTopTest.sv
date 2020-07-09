157e9b89d29dd10721f34be657034214acf90d99
alenkruth <alenkruth@gmail.com>
2020-07-09 14:22:52 +0530

    updated testbench for CTR mode

--- tb/AESTopTest.sv
+++ tb/AESTopTest.sv
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////
-// Author      : Alenkruth
-// Language    : SystemVerilog
-// Description : A simple testbench to test the functioning of the core in ECB
-//               mode
-///////////////////////////////////////////////////////////////////////////////               
+/////////////////////////////////////////////////////////////////////////////////
+// Author      : Alenkruth                                                     //
+// Language    : SystemVerilog                                                 //
+// Description : A simple testbench to test the functioning of the core in CTR //
+//               mode                                                          //
+/////////////////////////////////////////////////////////////////////////////////               
 module AESTopTest(
 
     );
@@ -49,16 +49,16 @@ module AESTopTest(
         enable = 1'b1;
         ///////////////////////////////////////////////////////
         // test vectors from the AES specfication NIST.FIPS 197
-        // plaintext_test = 128'h00112233445566778899aabbccddeeff;
-        // key_test = 256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
-        // the cipher text is 8ea2b7ca516745bfeafc49904b496089
+        plaintext_test = 128'h00112233445566778899aabbccddeeff;
+        key_test = 256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
+        // the cipher text is e9d2cdb9f661359178ed366dfa3a4671
         //////////////////////////////////////////////////////
 
         ///////////////////////////////////////////////////////
         // custom test vectors
-        plaintext_test = 128'h616c656e6b72757468616c656e6b7275; // alenkruthalenkru in ASCII
-        key_test = 256'h7465737474657374746573747465737474657374746573747465737474657374; // testtesttesttesttesttesttesttest in ASCII
-        // the cipher text is 4419ce8172f99fa38dc6119260edb3f8
+        // plaintext_test = 128'h616c656e6b72757468616c656e6b7275; // alenkruthalenkru in ASCII
+        // key_test = 256'h7465737474657374746573747465737474657374746573747465737474657374; // testtesttesttesttesttesttesttest in ASCII
+        // the cipher text is 70cc2ae4e73a187a7841073c04adcb09
         ///////////////////////////////////////////////////////
         #4000;
         $stop;
--- tb/README.md
+++ tb/README.md
@@ -1,8 +1,9 @@
 # AESCore <br/>
 ## Testbench <br/>
-AESTopTest is a simple test bench which simulates encryption of a 128bit plaintext. <br/>
+AESTopTest is a simple test bench which simulates encryption of a 128bit plaintext in CTR Mode. <br/>
+The module does not use a nonce but can be modified to use one. As of now a slice of the Key in used as an IV.
 ModelSim-Intel FPGA Starter Edition was used for simulation. <br/>
 ## VCD Files <br/>
-[TestVector20.vcd](https://github.com/Alenkruth/AESCore/tree/master/tb) contains the wave dump for the plaintext and key combination example provided in the [AES Documentation](https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197.pdf). <br/>
-[CustomTestVector20.vcd](https://github.com/Alenkruth/AESCore/tree/master/tb) contains the wave dump for a random plaintext and key combination.<br/>
+[TestVector.vcd](https://github.com/Alenkruth/AESCore/tree/master/tb) contains the wave dump for the plaintext and key combination example provided in the [AES Documentation](https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197.pdf). <br/>
+[CustomTestVector.vcd](https://github.com/Alenkruth/AESCore/tree/master/tb) contains the wave dump for a random plaintext and key combination.<br/>
 
